329e3652de9476f425b35b49faaf4f0239b92398
unknown <felipe@agenciasimetria.com.br>
2021-01-12 21:19:18 -0300

    instalação sqlite3 e feito testes

--- src/database/db.js
+++ src/database/db.js
@@ -4,4 +4,63 @@ const sqlite3 = require("sqlite3").verbose();
 //criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
+module.exports = db;
+
 //utilizar o objeto de dados para nossas operações
+//db.serialize(() =>{
+    //criar uma tabela com comandos sql
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS couples (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         name TEXT,
+    //         idHusband INTEGER,
+    //         idWife INTEGER,
+    //         date TEXT
+    //     );
+    // `)
+
+    // //inserir dados na tabela
+    // const query = `
+    //     INSERT INTO couples (
+    //         name,
+    //         idHusband,
+    //         idWife,
+    //         date
+    //     ) VALUES (?,?,?,?);
+    // `
+    // const values = [
+    //     "Navarro",
+    //     "3",
+    //     "4",
+    //     "12/01/2021"
+    // ]
+
+    // function afterInsertData(err){
+    //     if(err){
+    //         return console.log(err);
+    //     }
+    //     console.log("Couple Cadastrado com Sucesso");
+    //     console.log(this);
+    // }
+
+    // db.run(query, values, afterInsertData);
+
+    // consultar os dados da tabela
+    // db.all(`SELECT * FROM couples`, function(err, rows){
+    //    if(err){
+    //        return console.log(err);
+    //     }
+    //     console.log("Aqui estão seus Registros");
+    //     console.log(rows);
+    // })
+
+    //deletar os dados da tabela
+    //db.run(`DELETE FROM couples WHERE id = ?`, [1], function(err){
+    //    if(err){
+    //        return console.log(err);
+    //    }
+    //    console.log("Registro deletado com sucesso");        
+    //})
+
+    //editar os dados da tabela
+//});
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const server = express();
 
+//pegar o banco de dados
+const db = require("./database/db")
+
 //configurar pastas publicas
 server.use(express.static("public"));
 
@@ -14,6 +17,22 @@ nunjucks.configure("src/views", {
 //pagina inicial
 server.get("/", (req, res) => {
     return res.render("login.html", {  });
+});
+
+//consultar couples
+server.get("/couples", (req, res) => {
+
+    //pegar os dados do banco
+    db.all(`SELECT * FROM couples`, function(err, rows){
+       if(err){
+           return console.log(err);
+        }      
+        const total = rows.length;
+
+        //mostrar pg html com os dados do banco
+        return res.render("login.html", { couples: rows, total });
+    })
+    
 });
 
 server.listen(3000);
