f226e60105c9135d8098e81fafdafe59b8a816e3
Felipe Otelakoski <felipe@agenciasimetria.com.br>
2021-01-17 16:46:48 -0200

    iniciando o projeto

--- src/server.js
+++ src/server.js
@@ -23,16 +23,38 @@ server.get("/", (req, res) => {
 server.get("/couples", (req, res) => {
 
     //pegar os dados do banco
-    db.all(`SELECT * FROM couples`, function(err, rows){
-       if(err){
-           return console.log(err);
-        }      
-        const total = rows.length;
+    // db.all(`SELECT * FROM couples`, function(err, rows){
+    //    if(err){
+    //        return console.log(err);
+    //     }      
+    //     const total = rows.length;
 
-        //mostrar pg html com os dados do banco
-        return res.render("login.html", { couples: rows, total });
-    })
+    //     //mostrar pg html com os dados do banco
+    //     return res.render("login.html", { couples: rows, total });
+    // })
     
+});
+
+//pg de cadastro couples
+server.get("/cadastro", (req, res) => {
+    return res.render("cadastro.html", { couples: rows, total });        
+});
+
+//cadastrar no banco
+
+server.post("/cadastrar", (req, res) =>{
+    const DadosParaCadastar = req.body();
+    console.log(DadosParaCadastar);
+    //cadastrar os dados do banco
+    // db.run(`INSERT INTO couples (name, idHusband, idWife, date) VALUES (?,?,?,?)`, 
+    //     function(err, rows){
+    //         if(err){
+    //         return console.log(err);
+    //      }      
+          
+    //      //mostrar pg html com os dados do banco
+    //      return res.send( { message: "cadastrado com sucesso", couple: rows}  )
+    // })
 });
 
 server.listen(3000);
